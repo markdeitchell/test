07f525f96f82af9998a6cf0439562ef546ce6dc4
It Dterra <it@dterra.eu>
2023-05-12 15:27:43 +0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.de870e2905e88b5866654e36f7834027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a963b58c93c42fef186",
+    "url": "/test/static/css/main.34088196.chunk.css"
+  },
+  {
+    "revision": "9a963b58c93c42fef186",
+    "url": "/test/static/js/main.9a963b58.chunk.js"
+  },
+  {
+    "revision": "bec0bd21dfea5d8c3f53",
+    "url": "/test/static/css/1.85eb9311.chunk.css"
+  },
+  {
+    "revision": "bec0bd21dfea5d8c3f53",
+    "url": "/test/static/js/1.bec0bd21.chunk.js"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "7d684a9d387608d9437295e5639c28cb",
+    "url": "/test/index.html"
+  }
+];
